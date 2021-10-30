295bcd6b9412196341301925e35904dace85cadf
Mirza Abdul Awal <awal.mirza2016@gmail.com>
2021-10-30 15:59:13 -0400

    component file updated

---
+++ src/components/About/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './style.css';
+import coverImage from "./images/cover-image.png";
+
+
+function About() {
+      return (
+            <section className="bg-light about-page" id="about">
+                  <div className="container">
+                        <div className="row justify-content-center">
+                              <div className="col-lg-9 text-center">
+                                    <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+                                    <h2 className="text-center mt-0 about">About Me</h2>
+                                    <hr className="divider my-4" color="#408eff" />
+                                    <p className="text-muted mb-0">Mirza Abdul Awal - A full Stack Web Developer with a strong background in complete software development process. I have equal expertise in front end and as well as backend. My main language of work is JavaScript. Before becoming a full stack developer, I have the experience of working as Quality Assurance Analyst with the adequate skill on automation and API testing covering noth web and mobile applications</p>
+                                    <p></p>
+                                    <p className="text-muted mb-0">As a Front end developer, I have the skill on React, JavaScript, HTML, CSS, node.js and Inquirer. Using these experience I have created applications like "Password Generator", "Quiz Code", "Workday Schedule" and "Weather API". I also worked on a project "Kukoo's Library" using third party APIs with my team and deployed the application successfully</p>
+                                    <p></p>
+                                    <p className="text-muted mb-0">As a back end developer, I have the skills on Express.js, SQL and Handlebars, and using the OOps, ORM and MVC concept created API. As a result I could successfully complete a project name "Music World E-commerce" with my team and deployed using Heroku.</p>
+                                    <p></p>
+                                    <p className="text-muted mb-0">I also have the skill on working with MangoosDB, and Progressive Web Application development.</p>
+                                    <p></p>
+                                    <p className="text-muted mb-0">Currently I am working as Software Quality Assurance Analyst with ARC-One Solutions, where I have an unique opportunity to work with a highly skilled front end and back end developers and develop a timely software for American Red Cross and One Blood.</p>
+                                    <hr></hr>
+                                    <div className="col-sm-12">
+                                          <a href="https://github.com/mirzadev" className="btn btn-dark about-btn btn-outline-light" target="blank" role="button"><i className="fab fa-github"></i> Github</a>
+                                    </div>
+                              </div>
+                        </div>
+                  </div>
+            </section>
+      )
+}
+
+export default About;
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,166 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { SwipeableDrawer, Toolbar, AppBar, Typography, Button, IconButton, ListItem, ListItemIcon, ListItemText, List, Divider, } from '@material-ui/core';
+import { GetAppRounded, EmailRounded, GitHub, LinkedIn, Phone, Home, InfoRounded, ContactMailRounded } from '@material-ui/icons/';
+import MenuIcon from "@material-ui/icons/Menu";
+import Resume from "./Resume.pdf"
+import useStyles from './style';
+import './style.css';
+import Profile from './images/profilepic.jpg';
+
+
+
+function Navigation(props) {
+      const [clicked, setClicked] = useState(false);
+      const classes = useStyles();
+      const InfoList = [
+
+            {
+                  text: 'awal.mirza2016@gmail.com',
+                  icon: <EmailRounded />,
+                  link: "awal.mirza2016@gmail.com",
+            },
+            {
+                  text: 'GitHub',
+                  icon: <GitHub />,
+                  link: "https://github.com/mirzadev",
+            },
+            {
+                  text: 'LinkedIn',
+                  icon: <LinkedIn />,
+                  link: "https://www.linkedin.com/in/mirza-awal-5972511b5/",
+            },
+            {
+                  text: 'Phone',
+                  icon: <Phone />,
+                  link: "9545135171",
+            },
+
+      ]
+
+
+
+      return (
+            <div>
+
+
+                  <AppBar className={classes.appBar} position="static" color="inherit">
+
+                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={() => { setClicked(true) }}>
+                              <MenuIcon />
+                        </IconButton>
+                        <div className={classes.brandContainer}>
+                              <Button component={Link} to="/ProjectWithReact">
+                                    <a className="navbar-brand" id="brandName" href="/">
+                                          <img id="my-picture" src={Profile} alt="" height="75" width="75"></img >Mirza Abdul Awal - Full Stack Developer</a>
+
+                              </Button>
+                        </div>
+                        <div>
+                        </div>
+                        <Toolbar className={classes.toolbar} >
+                              <div className={classes.profile}>
+                                    <Typography className={classes.heading} align="left">
+                                          <Button className={classes.button} id="component-item" component={Link} to="/about">About</Button>
+                                    </Typography>
+                                    <Typography className={classes.heading} align="left">
+                                          <Button className={classes.button} id="component-item" component={Link} to="/project">Project</Button>
+                                    </Typography>
+                                    <Typography className={classes.heading} align="left">
+                                          <Button className={classes.button} id="component-item" component={Link} to="/contact">Contact</Button>
+                                    </Typography>
+                                    <Typography className={classes.heading} align="left">
+                                          <Button className={classes.button} id="component-item" href={Resume}>Resume</Button>
+                                    </Typography>
+                              </div>
+                        </Toolbar>
+                  </AppBar>
+                  <SwipeableDrawer
+                        anchor="left"
+                        open={clicked}
+                        onClose={() => { setClicked(false) }}
+                        onOpen={() => { }}
+                        className={classes.drawer}>
+                        {/* <a href="/ProjectWithReact"
+                              className={classes.roundedPic}>
+                              <Avatar
+                                    alt="Profile pic"
+                                    src={Profile}
+                                    className={classes.roundedPic}
+                                    id="avatar" />
+                        </a> */}
+                        <List>
+                              {InfoList.map((item, index) => {
+                                    const { text, icon, onClick, link } = item;
+                                    return (
+                                          <a href={link} target="_blank" rel="noopener noreferrer" primary={text} style={{ color: "#8B0000", textDecoration: "none" }} >
+                                                <ListItem button key={text} onClick={onClick}>
+                                                      {icon && <ListItemIcon>{icon}</ListItemIcon>}
+                                                      <ListItemText primary={text} />
+                                                </ListItem>
+                                          </a>
+                                    )
+                              })}
+                              <ListItem>
+                                    <ListItemIcon>
+                                          <Phone />
+                                    </ListItemIcon>
+                                    <ListItemText primary="(954)555-6666" />
+                              </ListItem>
+                        </List>
+                        <Divider />
+                        <List>
+                              <Link to="/ProjectWithReact" style={{ color: "#000000", textDecoration: "none" }}>
+                                    <ListItem button key="Home">
+                                          <ListItemIcon>
+                                                <Home />
+
+                                          </ListItemIcon>
+                                          <ListItemText primary="Home" />
+                                    </ListItem>
+                              </Link>
+                        </List>
+                        <List>
+                              <Link to="/about" style={{ color: "#000000", textDecoration: "none" }}>
+                                    <ListItem button key="About">
+                                          <ListItemIcon>
+                                                <InfoRounded />
+                                          </ListItemIcon>
+                                          <ListItemText primary="About" />
+                                    </ListItem>
+                              </Link>
+                        </List>
+                        <List>
+                              <Link to="/contact" style={{ color: "#000000", textDecoration: "none" }}>
+                                    <ListItem button key="Contact">
+                                          <ListItemIcon>
+                                                <ContactMailRounded />
+                                          </ListItemIcon>
+                                          <ListItemText primary="Contact" />
+                                    </ListItem>
+                              </Link>
+                        </List>
+                        <Divider />
+                        <List>
+                              <a href={Resume} rel="noreferrer" style={{ color: "#000000", textDecoration: "none" }} >
+                                    <ListItem>
+                                          <ListItemIcon>
+                                                <GetAppRounded />
+                                          </ListItemIcon>
+                                          <ListItemText primary="Resume" />
+                                    </ListItem>
+                              </a>
+                        </List>
+                  </SwipeableDrawer>
+                  {/* <div className="my-5" >
+
+                        <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+                  </div > */}
+
+            </div>
+
+      )
+
+}
+
+export default Navigation;
